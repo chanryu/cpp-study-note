b8b215d9c3ac5265b6dc50cd0da883ba956bc0a0
Chan Ryu <upriser@gmail.com>
2025-05-01 08:40:23 +1000

    wip

--- topics/range/views.cpp
+++ topics/range/views.cpp
@@ -1,10 +1,16 @@
 #include <gtest/gtest.h>
+
 #include <ranges>
+#include <vector>
 
 TEST(ViewTests, FilterView) {
   auto numbers = std::vector{1, 2, 3, 4, 5};
-  auto even_numbers = numbers | std::views::filter([](int n) { return n % 2 == 0; });
-  std::vector actual(even_numbers.begin(), even_numbers.end());
-  std::vector expected = {2, 4};
+  auto even_numbers = numbers | std::views::filter([](auto n) { return n % 2 == 0; });
+
+  std::vector<int> actual(even_numbers.begin(), even_numbers.end());
+  // std::ranges::copy(even_numbers, std::back_inserter(actual));
+
+  auto const expected = std::vector{2, 4};
+
   EXPECT_EQ(actual, expected);
 }
