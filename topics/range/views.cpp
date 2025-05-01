a19ec7b23fcf381b5b541395eb18879608610310
Chan Ryu <upriser@gmail.com>
2025-05-01 15:55:18 +1000

    fix

--- topics/range/views.cpp
+++ topics/range/views.cpp
@@ -7,7 +7,7 @@ TEST(ViewsTests, _) {
   auto const actual = std::views::iota(1)                                     // generated stream of ints from 1
                       | std::views::filter([](auto n) { return n % 2 == 0; }) // filter even numbers
                       | std::views::drop(1)                                   // drop the first -- 2
-                      | std::views::take(3)                                   // take the next 3 -- 6, 8, 10
+                      | std::views::take(3)                                   // take the next 3 -- 4, 6, 8
                       | std::ranges::to<std::vector>();                       // convert to vector
 
   auto const expected = std::vector{4, 6, 8};
