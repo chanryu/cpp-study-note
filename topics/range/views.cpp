3f695f967517ed9087fa85c5097179d7cde01835
Chan Ryu <upriser@gmail.com>
2025-05-01 15:52:03 +1000

    clang-format

--- topics/range/views.cpp
+++ topics/range/views.cpp
@@ -3,11 +3,14 @@
 #include <ranges>
 #include <vector>
 
-TEST(ViewsTests, FilterView) {
-  auto numbers = std::views::iota(0, 10) | std::views::filter([](auto n) { return n % 2 == 0; });
+TEST(ViewsTests, _) {
+  auto const actual = std::views::iota(1)                                     // generated stream of ints from 1
+                      | std::views::filter([](auto n) { return n % 2 == 0; }) // filter even numbers
+                      | std::views::drop(1)                                   // drop the first -- 2
+                      | std::views::take(3)                                   // take the next 3 -- 6, 8, 10
+                      | std::ranges::to<std::vector>();                       // convert to vector
 
-  auto const actual = std::ranges::to<std::vector>(numbers);
-  auto const expected = std::vector{0, 2, 4, 6, 8};
+  auto const expected = std::vector{4, 6, 8};
 
   EXPECT_EQ(actual, expected);
 }
