d90b15cac81dcd6cdf91759a8fc1f398f8ec0fb1
Chan Ryu <upriser@gmail.com>
2025-05-01 23:02:57 +1000

    wip

--- topics/range/views.cpp
+++ topics/range/views.cpp
@@ -3,14 +3,15 @@
 #include <ranges>
 #include <vector>
 
-TEST(ViewsTests, _) {
+TEST(ViewsTests, views_showcase) {
   auto const actual = std::views::iota(1)                                     // generated stream of ints from 1
                       | std::views::filter([](auto n) { return n % 2 == 0; }) // filter even numbers
                       | std::views::drop(1)                                   // drop the first -- 2
                       | std::views::take(3)                                   // take the next 3 -- 4, 6, 8
+                      | std::views::transform([](auto n) { return n * 2; })   // double the numbers
                       | std::ranges::to<std::vector>();                       // convert to vector
 
-  auto const expected = std::vector{4, 6, 8};
+  auto const expected = std::vector{8, 12, 16};
 
   EXPECT_EQ(actual, expected);
 }
