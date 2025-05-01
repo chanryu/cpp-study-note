c76ad71c5c9ee1a7c89fe7faba4752954f47363f
Chan Ryu <upriser@gmail.com>
2025-05-01 23:09:27 +1000

    Project setup 2 (#2)

--- topics/range/views.cpp
+++ topics/range/views.cpp
@@ -1,10 +1,17 @@
 #include <gtest/gtest.h>
-#include <ranges>
 
-TEST(ViewTests, FilterView) {
-  auto numbers = std::vector{1, 2, 3, 4, 5};
-  auto even_numbers = numbers | std::views::filter([](int n) { return n % 2 == 0; });
-  std::vector actual(even_numbers.begin(), even_numbers.end());
-  std::vector expected = {2, 4};
+#include <ranges>
+#include <vector>
+
+TEST(ViewsTests, views_showcase) {
+  auto const actual = std::views::iota(1)                                     // generated stream of ints from 1
+                      | std::views::filter([](auto n) { return n % 2 == 0; }) // filter even numbers
+                      | std::views::drop(1)                                   // drop the first -- 2
+                      | std::views::take(3)                                   // take the next 3 -- 4, 6, 8
+                      | std::views::transform([](auto n) { return n * 2; })   // double the numbers
+                      | std::ranges::to<std::vector>();                       // convert to vector
+
+  auto const expected = std::vector{8, 12, 16};
+
   EXPECT_EQ(actual, expected);
 }
